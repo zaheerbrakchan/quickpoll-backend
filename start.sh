a1ebcc208968ff88d2076c13687e07f62df42513
zaheerbrakchan <xaheer3scc@gmail.com>
2025-10-27 00:44:14 +0530

    Make start.sh executable

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ set -e
 
 python3 -m pip install --upgrade pip
 python3 -m pip install -r requirements.txt
-python3 -m uvicorn main:app --host 0.0.0.0 --port 8000
+python3 -m uvicorn app.main:app --host 0.0.0.0 --port 8000
+
