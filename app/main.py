5b0b9f5b9a7f17476b456a72cebc150dafb869eb
zaheerbrakchan <xaheer3scc@gmail.com>
2025-10-27 09:59:44 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes import polls, votes, likes, auth
 
 app = FastAPI(title="QuickPoll Backend")
@@ -9,6 +11,15 @@ app.include_router(polls.router, prefix="/api/polls", tags=["Polls"])
 app.include_router(votes.router, prefix="/api/votes", tags=["Votes"])
 app.include_router(likes.router, prefix="/api/likes", tags=["Likes"])
 app.include_router(auth.router, prefix="/api/auth", tags=["Authentication"])
+
+# Allow all origins (not recommended for production)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # 👈 allow all domains
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
